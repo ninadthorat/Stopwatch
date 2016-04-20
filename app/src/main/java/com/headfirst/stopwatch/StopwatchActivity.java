d0c96b5ef13ab7c6b4c7befe5abd03c3e4f484eb
ninad <ninadprouser@gmail.com>
2016-04-20 21:24:44 +0530

    bug fixed

--- app/src/main/java/com/headfirst/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/headfirst/stopwatch/StopwatchActivity.java
@@ -17,7 +17,7 @@ public class StopwatchActivity extends Activity{
     private boolean running;
     private boolean wasRunning;
     //small change nklj
-    kasdf;kjadslfk
+    //adding "bug fix"
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_stopwatch);
