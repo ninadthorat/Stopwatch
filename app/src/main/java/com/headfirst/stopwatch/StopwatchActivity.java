f8da2559a26e4ee9b8b0bc83f71fc2956691e2da
ninad <ninadprouser@gmail.com>
2016-04-20 15:26:40 +0530

    small change made

--- app/src/main/java/com/headfirst/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/headfirst/stopwatch/StopwatchActivity.java
@@ -16,7 +16,7 @@ public class StopwatchActivity extends Activity{
     private int seconds = 0;
     private boolean running;
     private boolean wasRunning;
-
+    //small change
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_stopwatch);
